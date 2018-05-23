668a8ced2b01d418413cc9432718b542bee32c86
arinanda <arinanda.online@gmail.com>
2018-05-24 01:15:11 +0700

    update
    Model and pkl, akurasi feature, response

--- Main.py
+++ Main.py
@@ -23,34 +23,13 @@ def predict():
         schema:
           id: Petal
           required:
-            - milk
             - toothed
-            - eggs
-            - feathers
-            - backbone
             - legs
             - breathes
-            - tail
-            - fins
+            - backbone
             - aquatic
           properties:
-            milk:
-              type: int
-              description: Please input with value 0 or 1.
-              default: 0
             toothed:
-              type: int
-              description: Please input with value 0 or 1.
-              default: 0
-            eggs:
-              type: int
-              description: Please input with value 0 or 1.
-              default: 0
-            feathers:
-              type: int
-              description: Please input with value 0 or 1.
-              default: 0
-            backbone:
               type: int
               description: Please input with value 0 or 1.
               default: 0
@@ -62,11 +41,7 @@ def predict():
               type: int
               description: Please input with value 0 or 1.
               default: 0
-            tail:
-              type: int
-              description: Please input with value 0 or 1.
-              default: 0
-            fins:
+            backbone:
               type: int
               description: Please input with value 0 or 1.
               default: 0
@@ -80,24 +55,19 @@ def predict():
     """
     new_task = request.get_json()
 
-    milk = new_task['milk']
     toothed = new_task['toothed']
-    eggs = new_task['eggs']
-    feathers = new_task['feathers']
     backbone = new_task['backbone']
     legs = new_task['legs']
     breathes = new_task['breathes']
-    tail = new_task['tail']
-    fins = new_task['fins']
     aquatic = new_task['aquatic']
 
-    X_New = np.array([[legs, toothed, backbone, tail, breathes, feathers, milk,aquatic, fins, eggs]])
+    X_New = np.array([[toothed, legs, breathes, backbone, aquatic]])
     # X_New = np.array([[0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 4, 0, 0, 0]])
     clf = joblib.load('randomForestClassifier.pkl')
 
     resultPredict = clf[0].predict(X_New)
 
-    return jsonify({'message': format(clf[1][resultPredict-1])})
+    return jsonify({'message': format(clf[1][resultPredict-1][0][0])})
 
 
 if __name__ == '__main__':
